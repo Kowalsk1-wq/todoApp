7885fafd6e8a31f3baaf5ed6ded44302ad7924ce
Hugo Matheus <limabrot879@gmail.com>
2020-02-10 16:18:02 -0300

    duas rotas faltando

--- src/app/controllers/TaskController.ts
+++ src/app/controllers/TaskController.ts
@@ -39,6 +39,93 @@ class TaskController {
     }
   }
 
+  async changeStatus(req: any, res: any) {
+    const { id } = req.params;
+    const { status } = await req.query.status;
+
+    if (id.length === 36 && typeof id === "string") {
+      const tasktarget = await getConnection()
+        .createQueryBuilder()
+        .select("task")
+        .from(Task, "task")
+        .where("id = :id", { id })
+        .getOne();
+
+      if (tasktarget) {
+        if (
+          status == "Pendente" ||
+          status == "Andamento" ||
+          status == "Concluida"
+        ) {
+          const taskChange = await getConnection()
+            .createQueryBuilder()
+            .update(Task)
+            .set({
+              status: () => `${status}`
+            })
+            .where("id = :id", { id })
+            .execute();
+
+          return res.json({
+            ok: `Tarefa com status ${status}!`,
+            task: taskChange
+          });
+        } else {
+          return res.json({
+            error: "Status Inválido!"
+          })
+        }
+      } else {
+        return res.json({
+          error: "Tarefa Não Encontrada!"
+        });
+      }
+    } else {
+      return res.json({
+        error: "Id Inválido!!"
+      });
+    }
+  }
+
+  async update(req: any, res: any) {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    if (id.length === 36 && typeof id === "string") {
+      const tasktarget = await getConnection()
+        .createQueryBuilder()
+        .select("task")
+        .from(Task, "task")
+        .where("id = :id", { id })
+        .getOne();
+
+      if (tasktarget) {
+        const taskUp = await getConnection()
+          .createQueryBuilder()
+          .update(Task)
+          .set({
+            description: () => `${description}`
+          })
+          .where("id = :id", { id })
+          .execute();
+
+        return res.json({
+          ok: "Tarefa Atualizada com Sucesso!",
+          antes: tasktarget,
+          agora: taskUp
+        });
+      } else {
+        return res.json({
+          error: "Tarefa Não Encontrada!"
+        });
+      }
+    } else {
+      return res.json({
+        error: "Id Inválido!!"
+      });
+    }
+  }
+
   async delete(req: any, res: any) {
     const { id } = req.params;
 
@@ -67,7 +154,7 @@ class TaskController {
     } else {
       return res.json({
         error: "Id Inválido!!"
-      })
+      });
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,8 @@ rt.get("/", (_req, res) => res.json({ Hello: true })); //<- Hello Route
 
 rt.get("/tasks", TaskController.list);
 rt.post("/tasks", TaskController.create);
+rt.put("/tasks/change/:id", TaskController.changeStatus);
+rt.put("/tasks/update/:id", TaskController.update);
 rt.delete("/tasks/:id", TaskController.delete);
 
 export default rt;
